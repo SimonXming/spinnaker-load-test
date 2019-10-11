8b9dd6627b4cc8e098967ef7a289d0f616c60d41
Your Name <you@example.com>
2019-10-11 01:29:40 -0700

    Update locustfile.py

--- src/spinnaker_load_test/locustfile.py
+++ src/spinnaker_load_test/locustfile.py
@@ -1,13 +1,39 @@
+
 from locust import HttpLocust, TaskSet, task
 
 class UserBehavior(TaskSet):
-    @task(1)
+    @task(10)
     def applications(self):
         self.client.get("/applications")
 
-    @task(2)
+    @task(10)
     def loadBalancers(self):
         self.client.get("/loadBalancers?provider=kubernetes")
+
+    @task(10)
+    def projects(self):
+        self.client.get("/projects")
+
+    @task(10)
+    def pipelineConfigs(self):
+        self.client.get("/pipelineConfigs")
+
+    @task(10)
+    def networks(self):
+        self.client.get("/networks")
+
+    @task(10)
+    def firewalls(self):
+        self.client.get("/firewalls")
+
+    @task(10)
+    def credentials(self):
+        self.client.get("/credentials")
+
+    @task(10)
+    def artifacts_credentials(self):
+        self.client.get("/artifacts/credentials")
+
 
 class WebsiteUser(HttpLocust):
     task_set = UserBehavior
